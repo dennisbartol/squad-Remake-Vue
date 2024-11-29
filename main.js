3614931d605d730fa2e8be14e8c8de3719ce41e6
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-11-29 21:02:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import AudioMixin from "@/mixins/audioMixin"; 
 
-
+const app = createApp(App);
+app.mixin(audioMixin); 
+app.mount("#app");
