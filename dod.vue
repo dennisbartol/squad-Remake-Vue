80e89db33bb4ac7f1eb3b92137f3b164ba4648a8
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-02 15:18:29 +0100

    Update dod.vue

--- dod.vue
+++ dod.vue
@@ -1,6 +1,22 @@
 <template>
-
-  
+  <div id="container">
+    <div id="top">
+        <img src="./img/dod/dayofnodefeat.gif" alt="Day of Defeat" />
+        <br />
+      <a href="target.htm">
+        <img src="./img/back.gif" alt="Back" />
+      </a>
+      <br />
+      <img src=/img/music1.gif" alt="Music Note" />
+      <a @click="playSound">
+        <img src="./img/music2.gif" alt="Play Music" />
+      </a>
+      <a click="stopSound">
+          <img src="./img/music4.gif" alt="Stop Music" />
+      </a>
+      
+    </div>
+  </div>
 </template>
 
 <script>
