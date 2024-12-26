b648f006a678fdba0ef41bed9e3b60507de76413
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-12-26 19:31:09 +0100

    Update intro.vue

--- intro.vue
+++ intro.vue
@@ -4,5 +4,54 @@
 <script>
 </script>
 
-<style>
-  </style>
+<style scoped>
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #000;
+  color: #b28a52;
+  font-family: Verdana, sans-serif;
+  font-size: 0.9em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+  width: 100%;
+}
+
+.container {
+  width: 100%;
+  max-width: 1000px;
+  min-width: 390px;
+  text-align: center;
+  padding: 20px;
+  border: 1px dotted #977c5a;
+}
+
+.content {
+  background-color: #000;
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #b28a52;
+}
+
+.link-img {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+}
+
+.footer-text {
+  margin-top: 10px;
+  font-size: 0.8em;
+}
+</style>
