29d726848f5aad7afda3d8c0cc21262ea0faf3ec
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-02-23 22:08:32 +0100

    Update toDo_target.vue

--- src/components/toDo_target.vue
+++ src/components/toDo_target.vue
@@ -1,4 +1,25 @@
+<template>
+</template>
 
+<script>
+  export default { 
+    methods: { 
+      playSound() {
+        const audio = this.$refs.firstSound; 
+        if (audio) {
+          audio.play();
+        }
+      },
+      stopSound() {
+        const audio = this.$refs.firstSound; 
+        if (audio) {
+          audio.pause();
+          audio.currentTime = 0; 
+        }
+      },        
+    },
+  };
+</script>
 
 
 <style scoped>
