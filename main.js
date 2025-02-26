040c004c178231bc8f4b4527d4e30bb2b06fe8c4
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2025-02-26 13:28:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from './router';
 import AudioMixin from "@/mixins/audioMixin"; 
 import './styles/globalstyles.css';
+import { createHead } from '@vueuse/head';
 
 const app = createApp(App);
 app.mixin(audioMixin); 
